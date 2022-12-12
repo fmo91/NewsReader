25c126c9f10c833668fbfb64169fbc59295a0a88
Fernando Martín Ortiz <ortizfernandomartin@gmail.com>
2022-12-12 19:31:43 -0300

    Add top bar (#2)
    * Remove extra print statements
    
    * Add a top bar to the app

--- app/src/main/java/com/example/newsreader/ui/home/news/News.kt
+++ app/src/main/java/com/example/newsreader/ui/home/news/News.kt
@@ -4,9 +4,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.newsreader.R
+import com.example.newsreader.ui.base.NewsTopBar
 import com.example.newsreader.ui.home.news.components.ArticleRow
 
 @Composable
@@ -16,15 +23,29 @@ fun News(
     val articles = viewModel.articlesFlow.collectAsState().value
 
     LaunchedEffect(key1 = Unit) {
-        println("About to launch effect for loading articles")
         viewModel.loadArticles()
-        println("Received articles")
     }
 
-    LazyColumn {
-        items(articles) { article ->
-            ArticleRow(article = article)
+    Container {
+        LazyColumn {
+            items(articles) { article ->
+                ArticleRow(article = article)
+            }
         }
     }
 }
 
+@Composable
+fun Container(
+    content: @Composable () -> Unit
+) {
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        topBar = {
+            NewsTopBar()
+        }
+    ) {
+        content()
+    }
+}
